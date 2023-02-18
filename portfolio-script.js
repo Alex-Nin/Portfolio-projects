941543207d6b0cfb67c16eb7966617ff57526387
Alex-Nin <122234932+Alex-Nin@users.noreply.github.com>
2023-02-18 15:00:30 -0700

    Add files via upload

---
+++ portfolio-script.js
@@ -0,0 +1,9 @@
+let navLinkList = document.getElementById("nav-list");
+
+function hideBtn() {
+    navLinkList.style.top = "-1500px";
+}
+
+function showBtn() {
+    navLinkList.style.top = "0";
+}
